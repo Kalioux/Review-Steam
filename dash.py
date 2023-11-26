ee786460d92415ff244bd03c8820e7971b3077de
Kaliel <71413734+Kalioux@users.noreply.github.com>
2023-11-26 15:33:24 -0300

    Update matplot

--- dash.py
+++ dash.py
@@ -161,4 +161,17 @@ st.write("### Porcentagem de Jogos Compatíveis com Steam Deck:")
 percentage_steam_deck = int((df['steam_deck'].sum() / len(df)) * 100)
 st.write(f"Aproximadamente {percentage_steam_deck}% dos jogos são compatíveis com Steam Deck.")
 
+import streamlit as st
+import matplotlib.pyplot as plt
+
+# Cria um gráfico de linhas simples
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+plt.plot(x, y)
+
+# Exibe o gráfico no Streamlit
+st.pyplot(plt.plot(x, y))
+
+
 # that´s all folks...
