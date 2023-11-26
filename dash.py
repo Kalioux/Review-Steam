81e2a37d94fa045ae40ce1b97452f3747dbd6f13
Kaliel <71413734+Kalioux@users.noreply.github.com>
2023-11-26 17:13:53 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -24,27 +24,19 @@ if pd.api.types.is_datetime64_any_dtype(df['date_release']):
     st.write(df)
 
    # Top 5 jogos mais caros
-st.write("### Top 5 Jogos Mais Caros:")
-expensive_games = df[df['price_final'] >= 190].sort_values('price_final', ascending=False).head(5)
+    st.write("### Top 5 Jogos Mais Caros:")
+    expensive_games = df[df['price_final'] >= 190].sort_values('price_final', ascending=False).head(5)
 
-# Criar gráfico de barras com linha de avaliação dos usuários
-chart_expensive_games = alt.Chart(expensive_games).mark_bar().encode(
-    x='price_final:Q',
-    y=alt.Y('title:N', sort='-x'),
-    color=alt.Color('title:N', scale=alt.Scale(scheme='viridis')),
-    tooltip=['title:N', 'price_final:Q']
-).configure_axis(
-    labels=False
-)
+    chart_expensive_games = alt.Chart(expensive_games).mark_bar().encode(
+        x='price_final:Q',
+        y=alt.Y('title:N', sort='-x'),
+        color=alt.Color('title:N', scale=alt.Scale(scheme='viridis')),  # Alterada para 'viridis'
+        tooltip=['title:N', 'price_final:Q']
+    ).configure_axis(
+        labels=False
+    )
 
-# Adicionar a linha de avaliação dos usuários
-rule = alt.Chart(expensive_games).mark_rule(color='red').encode(
-    y='title:N',
-    size=alt.value(3),
-    tooltip=['user_reviews:Q']
-)
-
-st.altair_chart(chart_expensive_games + rule, use_container_width=True)
+    st.altair_chart(chart_expensive_games, use_container_width=True) 
 
 
 
