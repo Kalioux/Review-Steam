b8e6bd30a4951d30fb8f0858a60d68bd180899bf
Kaliel <71413734+Kalioux@users.noreply.github.com>
2023-11-26 13:06:31 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 st.set_page_config(layout="wide")
-st.write(emojize('🕹️'))
+st.markdown('**🕹️**')
 # Carregar a base de dados
 df = pd.read_csv('games.csv')
 
